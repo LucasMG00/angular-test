e761aecff0a993f035667759454cce68e2acb1ac
Lucas MG <lu.mang.gon@gmail.com>
2022-07-26 15:55:28 -0300

    import update

--- src/app/components/todo.module.ts
+++ src/app/components/todo.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { StarComponent } from "../shared/components/stars/stars.component";
 import { StarsModule } from "../shared/components/stars/stars.module";
 import { TodoComponent } from "./todo.component";
 
