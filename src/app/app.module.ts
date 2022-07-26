949b5634ec22fe4977b9f6d6ba82f56b3c0e553b
Lucas MG <lu.mang.gon@gmail.com>
2022-07-26 15:47:58 -0300

    shared folder & module updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo.component';
-import { StarComponent } from './stars/stars.component';
+import { StarComponent } from './shared/components/stars/stars.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
+import { TodoModule } from './components/todo.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent,
-    StarComponent,
     NavBarComponent,
     Error404Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    TodoModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'todos', pathMatch: 'full'
-      },
-      {
-        path: 'todos', component: TodoComponent
       },
       {
         path: '**', component: Error404Component
